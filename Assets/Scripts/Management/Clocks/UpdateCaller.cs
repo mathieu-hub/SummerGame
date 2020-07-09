344102b534e34d566ed31ada68024e06461ff5d4
Xanthin <x.paris@rubika-edu.com>
2020-07-09 20:31:28 +0200

    Ajout Clock Tool (Nico)

---
+++ Assets/Scripts/Management/Clocks/UpdateCaller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace NCO.Clocks
+{
+    public class UpdateCaller : MonoBehaviour
+    {
+        public static UpdateCaller Instance = null;
+
+        //Event used to run the clocks (NCO_Clocks package)
+        public delegate void UpdateCall();
+        public static event UpdateCall OnUpdate;
+
+        private void Update()
+        {
+            OnUpdate?.Invoke();
+        }
+    }
+}

e716568ab409e8b227874add02106f2888b94835
Xanthin <x.paris@rubika-edu.com>
2020-07-09 20:48:50 +0200

    Start VegetableGarden + Ajout TextmeshPro

---
+++ Assets/Scripts/Production/GardenBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GardenBehaviour : MonoBehaviour
+{
+    /// <summary>
+    /// XP_This script makes the bahaviour of the Garden (Clock + Update Vegetable)
+    /// </summary>
+
+    #region Variables
+
+    public static Clock timer;
+    [Header("Clock")]
+    [Range(0.0F, 15.0F)]
+    public float duration;
+
+    [Header("Storage")]
+    public float storedVegetable;
+    #endregion
+
+    void Start()
+    {
+        timer = new Clock(duration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(timer.time);
+
+        if (timer.finished)
+        {
+            storedVegetable += 1;
+            timer = new Clock(duration);
+        }
+
+        if(storedVegetable == 6)
+        {
+            timer.Pause();
+        }
+    }
+}

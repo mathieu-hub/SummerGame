08944e3f4e26022f7180685e1912b8fc1a7f1f85
Xanthin <x.paris@rubika-edu.com>
2020-07-11 12:18:29 +0200

    Restarting a clock when get 6 productions

--- Assets/Scripts/Production/GardenBehaviour.cs
+++ Assets/Scripts/Production/GardenBehaviour.cs
@@ -22,10 +22,8 @@ namespace Production
         public float duration;
 
         [Header("VisualClock")]
-        [SerializeField]
-        private Image durationBar;
-        [SerializeField]
-        private Image durationBarBackground;
+        [SerializeField] private Image durationBar;
+        [SerializeField] private Image durationBarBackground;
         public TextMeshProUGUI storedCount;
 
         [Header("Storage")]
@@ -47,29 +45,26 @@ namespace Production
             //Increase Count
             if (timer.finished)
             {
-                storedVegetable += 1;
-                timer = new Clock(duration);
+                Production();
             }
             //Stop Production at 6
             if (storedVegetable == 6)
             {
                 timer.Pause();
             }
-            else
+            /*else   
             {
                 print(timer.time);
-            }
+            }*/
 
             if (Input.GetKeyDown(KeyCode.Space)){
-                
-                GameManager.Instance.vegetablesCount += storedVegetable;
-                storedVegetable = 0;
+                GetProduction();
             }
         }
 
+        #region Methods
         void UpdateUi()
         {
-
             storedCount.text = storedVegetable.ToString();
             durationBar.fillAmount = (float)timer.time / (float)duration;
 
@@ -79,5 +74,23 @@ namespace Production
 
             }
         }
+
+        void GetProduction()
+        {
+            if(storedVegetable == 6)
+            {
+                timer = new Clock(duration);
+            }
+
+            GameManager.Instance.vegetablesCount += storedVegetable;
+            storedVegetable = 0;
+        }
+
+        void Production()
+        {
+            storedVegetable += 1;
+            timer = new Clock(duration);
+        }
+        #endregion
     }
 }

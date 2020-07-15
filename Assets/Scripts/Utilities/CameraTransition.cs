9925710e775ce202d6cc4bb0052d9fa7a2653c0f
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-07-15 17:27:38 +0200

    Start Camera Scaling

---
+++ Assets/Scripts/Utilities/CameraTransition.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class CameraTransition : MonoBehaviour
+{
+    public CinemachineVirtualCamera shortCam;
+    public CinemachineVirtualCamera largeCam;
+    bool shortCamIsActivate = true;
+
+
+    // Transition de caméra 
+    private void Update()
+    {
+        if(shortCamIsActivate == true && Input.GetButtonDown("Y_Button"))
+        {
+            shortCam.gameObject.SetActive(false);
+            largeCam.gameObject.SetActive(true);
+            shortCamIsActivate = false;
+        }
+
+        if (shortCamIsActivate == false && Input.GetButtonDown("Y_Button"))
+        {
+            largeCam.gameObject.SetActive(false);
+            shortCam.gameObject.SetActive(true);
+            shortCamIsActivate = true;
+        }
+    }  
+}

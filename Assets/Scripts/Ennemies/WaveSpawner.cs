794012ddbc37a248eca19934cd7acc6b0e021b22
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-07-16 18:58:12 +0200

    Create infinity Waves With Differents Difficult + Add WaveCountdownTimer

--- Assets/Scripts/Ennemies/WaveSpawner.cs
+++ Assets/Scripts/Ennemies/WaveSpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Ennemies
 {
@@ -8,25 +9,46 @@ namespace Ennemies
 	{
         [SerializeField]
         private Transform ennemyPrefab;
+
+        [SerializeField]
+        private Transform spawnPoint;
+
         [SerializeField]
         private float timeBreak = 20f;
 
         private float countdown = 2f;
 
+        [SerializeField]
+        private Text waveCountdownTimer;
+
+        private int waveIndex = 0;
+
 		void Update()
 		{
 			if (countdown <= 0f)
             {
-                SpawnWave();
+                StartCoroutine (SpawnWave());
                 countdown = timeBreak;
             }
 
             countdown -= Time.deltaTime;
+            waveCountdownTimer.text = Mathf.Floor(countdown).ToString();
 		}
 
-        void SpawnWave()
+        IEnumerator SpawnWave()
         {
-            Debug.Log("une nouvelle vague apparaît");
+            waveIndex++;
+
+            for (int i = 0; i < waveIndex; i++)
+            {
+                SpawnEnnemy();
+                yield return new WaitForSeconds(0.3f);
+            }
+        }
+
+        void SpawnEnnemy()
+        {
+            Instantiate(ennemyPrefab, spawnPoint.position, spawnPoint.rotation) ;
         }
 	}
 }

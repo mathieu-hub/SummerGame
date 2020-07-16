e503e77cbc85431b005960fdf28d1a5482dd34cf
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-07-16 18:09:32 +0200

    Start WaveManager

---
+++ Assets/Scripts/Ennemies/WaveSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ennemies
+{
+	public class WaveSpawner : MonoBehaviour
+	{
+        [SerializeField]
+        private Transform ennemyPrefab;
+        [SerializeField]
+        private float timeBreak = 20f;
+
+        private float countdown = 2f;
+
+		void Update()
+		{
+			if (countdown <= 0f)
+            {
+                SpawnWave();
+                countdown = timeBreak;
+            }
+
+            countdown -= Time.deltaTime;
+		}
+
+        void SpawnWave()
+        {
+            Debug.Log("une nouvelle vague apparaît");
+        }
+	}
+}
+

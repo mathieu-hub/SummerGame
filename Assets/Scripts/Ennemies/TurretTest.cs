55ef3512023b7cb8df7c2f2130eeb866495fe6f8
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-07-17 15:04:51 +0200

    Add TurretTest to prototype ennemies takeDamage

---
+++ Assets/Scripts/Ennemies/TurretTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ennemies
+{
+	public class TurretTest : MonoBehaviour
+	{
+        public Transform target;
+        public float range = 15f;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+            InvokeRepeating("UpdateTarget", 0f, 0.5f);
+		}
+
+        void UpdateTarget()
+        {
+            GameObject[] ennemies = GameObject.FindGameObjectsWithTag("Ennemy");
+            float shortestDistance = Mathf.Infinity;
+            GameObject nearestEnnemy = null;
+
+            foreach (GameObject ennemy in ennemies)
+            {
+                float distanceToEnnemy = Vector3.Distance(transform.position, ennemy.transform.position);
+                if (distanceToEnnemy < shortestDistance)
+                {
+                    shortestDistance = distanceToEnnemy;
+                    nearestEnnemy = ennemy;
+                }
+            }
+
+            if(nearestEnnemy != null && shortestDistance <= range)
+            {
+                target = nearestEnnemy.transform;
+            }
+            else
+            {
+                target = null;
+            }
+        }
+
+		// Update is called once per frame
+		void Update()
+		{
+			
+		}
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, range);
+        }
+    }
+}
+

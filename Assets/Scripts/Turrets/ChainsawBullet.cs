6ed2948f51b9a4f0d502f8473d34bcabe4ed04cb
Charles Bentein <c.bentein@rubika-edu.com>
2020-07-23 15:47:48 +0200

    Tronçoronce one time damage, and push different from lifetime

--- Assets/Scripts/Turrets/ChainsawBullet.cs
+++ Assets/Scripts/Turrets/ChainsawBullet.cs
@@ -9,21 +9,29 @@ namespace Turret
 	{
         #region Variables
         Rigidbody2D enemyRB;
+        [SerializeField] private float pushTime;
         #endregion
 
         protected override void DealDamage(GameObject enemy)
         {
             enemyHit = enemy.GetComponent<EnnemiesHealth>();
-            enemyHit.TakeDammage(damage);
             enemyRB = enemy.GetComponent<Rigidbody2D>();
+
+            enemyHit.TakeDammage(damage);
+            
             StartCoroutine(PushEnemy(enemyRB));
         }
 
         IEnumerator PushEnemy(Rigidbody2D _enemyRB)
         {
             _enemyRB.velocity = myRB.velocity;
-            yield return new WaitUntil(() => lifetime <= 0);
+            yield return new WaitForSeconds(pushTime);
             _enemyRB.velocity = Vector2.zero;
+        }
+
+        private void OnDestroy()
+        {
+            enemyRB.velocity = Vector2.zero;
         }
     }
 }

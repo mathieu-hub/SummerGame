9eb5d3a2e881694693dd10ecf6d32752f643601f
Charles Bentein <c.bentein@rubika-edu.com>
2020-07-27 17:26:03 +0200

    Added Mortarbullet script | Neutered Turrets awake radius

---
+++ Assets/Scripts/Turrets/MortarBullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Turret
+{
+	public class MortarBullet : Bullet
+	{
+		#region Variables
+		
+		#endregion
+
+		
+	}
+}
+

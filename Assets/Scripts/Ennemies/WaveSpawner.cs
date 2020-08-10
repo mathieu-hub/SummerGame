20734a3e6b141e19368aca8d54cda6718b46b069
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-08-10 17:15:06 +0200

    Start To Upgrade WaveSpawner Script

--- Assets/Scripts/Ennemies/WaveSpawner.cs
+++ Assets/Scripts/Ennemies/WaveSpawner.cs
@@ -8,7 +8,7 @@ namespace Ennemies
 	public class WaveSpawner : MonoBehaviour
 	{
         [SerializeField]
-        private Transform ennemyPrefab;
+        private Transform walker;
 
         [SerializeField]
         private Transform spawnPoint;
@@ -16,28 +16,41 @@ namespace Ennemies
         [SerializeField]
         private float timeBreak = 20f;
 
+        [SerializeField]
         private float countdown = 2f;
 
         [SerializeField]
         private Text waveCountdownTimer;
 
+        [SerializeField]
         private int waveIndex = 0;
 
-		void Update()
-		{
-			if (countdown <= 0f)
-            {
-                StartCoroutine (SpawnWave());
-                countdown = timeBreak;
-            }
+        [SerializeField]
+        private bool waveInProgress = false;
 
-            countdown -= Time.deltaTime;
-            waveCountdownTimer.text = Mathf.Round(countdown).ToString();
+
+
+        void Update()
+		{
+            if (!waveInProgress)
+            {
+                if (countdown <= 0f)
+                {
+                    StartCoroutine(SpawnWave());
+                    countdown = timeBreak;
+                }
+
+                //Fonctionnement du compte à rebourd 
+                countdown -= Time.deltaTime;
+                waveCountdownTimer.text = Mathf.Round(countdown).ToString();
+            }
+			
 		}
 
         IEnumerator SpawnWave()
         {
             waveIndex++;
+            waveInProgress = true;
 
             for (int i = 0; i < waveIndex; i++)
             {
@@ -46,9 +59,10 @@ namespace Ennemies
             }
         }
 
+        //Permet de gérer les règles d'apparitions ennemis et de compositions de vagues.
         void SpawnEnnemy()
         {
-            Instantiate(ennemyPrefab, spawnPoint.position, spawnPoint.rotation) ;
+            Instantiate(walker, spawnPoint.position, spawnPoint.rotation) ;
         }
 	}
 }

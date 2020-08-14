79d6c6bf628831526d447a0e29ccbdbb229572ae
Xanthin <x.paris@rubika-edu.com>
2020-08-14 09:53:46 +0200

    Ajout Xanthin Works dans la scene Dev_01 // 90% Done

--- Assets/Scripts/UI/GameCanvasManager.cs
+++ Assets/Scripts/UI/GameCanvasManager.cs
@@ -7,7 +7,7 @@ namespace GameCanvas{
     public class GameCanvasManager : Singleton<GameCanvasManager>
     {
         public ScreenFade blackScreen = null;
-        public UpdateStructure updateStructure = null;
+        //public UpdateStructure updateStructure = null;
         
  
         private void Awake()

5a1f3c7973725da7e2c9689210180110735f19b8
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-08-15 13:24:16 +0200

    Creat and Add Crosspoints Script on each Crosspoint

---
+++ Assets/Scripts/Ennemies/Ways/Crosspoints.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ennemies;
+
+public class Crosspoints : MonoBehaviour
+{
+    public bool isOpen = false;
+}
+

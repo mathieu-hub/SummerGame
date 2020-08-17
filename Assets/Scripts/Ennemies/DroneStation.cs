5295fdf813e7ebafd3bde9c36de693b9042f7f44
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-08-17 15:23:04 +0200

    DroneStation

---
+++ Assets/Scripts/Ennemies/DroneStation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ennemies
+{
+	public class DroneStation : MonoBehaviour
+	{
+        
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.name == "Drone")
+            {
+                Debug.Log("un drone est entré");
+            }
+        }
+    }
+}
+

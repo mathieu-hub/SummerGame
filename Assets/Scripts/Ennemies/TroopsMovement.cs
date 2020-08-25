49665025a5d1f1a43b122bba1d3aa628d8959c19
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-08-25 19:20:51 +0200

    Update Troops Movement

--- Assets/Scripts/Ennemies/TroopsMovement.cs
+++ Assets/Scripts/Ennemies/TroopsMovement.cs
@@ -13,6 +13,11 @@ namespace Ennemies
         [SerializeField] private float initialspeed;
         [SerializeField] private float stopSpeed = 0f;
 
+        [SerializeField] private Transform targetMovement;
+        public Transform player;
+        public Transform siloPoint;
+        public GameObject baseArea;
+
         //[Header("Animator")]
         //public Animator animator;
 
@@ -23,20 +28,30 @@ namespace Ennemies
 
         void Start()
 		{
-			
+			if (baseArea.GetComponent<DetectionPlayer>().playerInBase == true)
+            {
+                targetMovement = player;
+            }
+            else if (baseArea.GetComponent<DetectionPlayer>().playerInBase == false)
+            {
+                targetMovement = siloPoint;
+            }
 		}
 
 		void Update()
 		{
-			
-		}
+            //Movements
+            Vector3 direction = targetMovement.position - transform.position;
+            transform.Translate(direction.normalized * speed * Time.deltaTime);            
+        }
 
         void InitialisationValues()
         {
             initialspeed = 10f;
             speed = initialspeed;
             //animator.SetBool("isDrone", true);
-        }
+        }        
+        
     }
 }
 

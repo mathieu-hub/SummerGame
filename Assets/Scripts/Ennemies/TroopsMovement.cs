5a2c2b1a4e53462f313ec14137a0613c504566d7
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-08-25 17:18:38 +0200

    Start Trooper Movement

---
+++ Assets/Scripts/Ennemies/TroopsMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ennemies 
+{
+	public class TroopsMovement : MonoBehaviour
+	{
+        public bool isNotDrone;
+
+        [Header("Values")]
+        [SerializeField] private float speed;
+        [SerializeField] private float initialspeed;
+        [SerializeField] private float stopSpeed = 0f;
+
+        //[Header("Animator")]
+        //public Animator animator;
+
+        void Awake()
+        {
+            InitialisationValues();            
+        }
+
+        void Start()
+		{
+			
+		}
+
+		void Update()
+		{
+			
+		}
+
+        void InitialisationValues()
+        {
+            initialspeed = 10f;
+            speed = initialspeed;
+            //animator.SetBool("isDrone", true);
+        }
+    }
+}
+

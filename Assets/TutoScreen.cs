88e7a572a458e61f548e81d0e1f309d1d1a21952
Xanthin <x.paris@rubika-edu.com>
2020-08-28 14:58:55 +0200

    Menu Working

--- Assets/TutoScreen.cs
+++ Assets/TutoScreen.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class TutoScreen : MonoBehaviour
 {
@@ -26,6 +27,11 @@ public class TutoScreen : MonoBehaviour
 
     private Color startcolor;
 
+    [Header("Cancel")]
+    [SerializeField] private Image cancelCircle;
+    public float cancelTime;
+    public bool canCancel = false;
+
     private void Start()
     {
         startcolor = leftArrowInt.color;
@@ -33,11 +39,20 @@ public class TutoScreen : MonoBehaviour
         leftArrowExt.enabled = false;
         leftArrowInt.enabled = false;
 
+        text[0] = "In this game you play as an alien named Bolb." +  " He lives peacefully in the company of his animals and vegetables.";
+        text[1] = "However, one day, the ORS (Organisation of Space Research) discovered Bolb’s farm on a Saturn's satellite.";
+        text[2] = "Bolb does not like to be disturbed. To keep his existence secret, Bolb will have to use his plants to defend himself.";
+        text[3] = "Nevertheless the production of Bolb cannot stop facing these waves of assailants. Three resources will allow Bolb to build a worthy defense.";
+        text[4] = "The ORS will not be doing any research and will be able to call for reinforcements. However, Bolb, is not alone. He can count on his friend Flint, the salesman.";
+        text[5] = "Help Bolb keep his life peaceful.";
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        cancelCircle.fillAmount = cancelTime / 100f;
 
         if(currentState == 0)
         {
@@ -64,11 +79,16 @@ public class TutoScreen : MonoBehaviour
 
 
 
-        ImageView = images[currentState];
+        //Bon
+        ImageView.sprite = images[currentState].GetComponent<Image>().sprite; ;
+
+
         textView.text = text[currentState];
 
+        Debug.Log(ImageView.sprite);
 
-        if(Input.GetAxisRaw("Left_Joystick_X") > 0.15 && !changing)
+
+        if(Input.GetAxisRaw("Left_Joystick_X") > 0.15 && !changing && !canCancel)
         {
             changing = true;
             Modifier = 1;
@@ -76,7 +96,7 @@ public class TutoScreen : MonoBehaviour
             rightArrowInt.color = Color.black;
 
         }
-        if (Input.GetAxisRaw("Left_Joystick_X") < -0.15 && !changing)
+        if (Input.GetAxisRaw("Left_Joystick_X") < -0.15 && !changing && !canCancel)
         {
             changing = true;
             Modifier = -1;
@@ -90,33 +110,62 @@ public class TutoScreen : MonoBehaviour
             Change();
         }
 
-        void Change()
+        if(Input.GetButtonDown("B_Button") && changing == false)
         {
+            canCancel = true;
+            
 
-            rightArrowInt.color = startcolor;
-            leftArrowInt.color = startcolor;
+            
+        }
 
-            changing = false;
-            needToChange = false;
+        if (canCancel)
+        {
+            cancelTime += 0.1f;
+        }
 
-            if(currentState != images.Length-1)
-            {
-                currentState += Modifier;
-            }
 
-            if(currentState == images.Length-1 && Modifier == -1)
-            {
-                currentState += Modifier;
-            }
+        if (Input.GetButtonUp("B_Button"))
+        {
+            canCancel = false;
+            cancelTime = 0f;
+        }
 
-            Modifier = 0;
+        if(cancelTime >= 100)
+        {
+            cancelTime = 0;
+            currentState = 0;
+            SceneManager.LoadScene(0);
+        }
 
-           
+        
+    }
 
-            if (currentState < 0)
-            {
-                currentState = 0;
-            }
+    void Change()
+    {
+
+        rightArrowInt.color = startcolor;
+        leftArrowInt.color = startcolor;
+
+        changing = false;
+        needToChange = false;
+
+        if (currentState != images.Length - 1)
+        {
+            currentState += Modifier;
+        }
+
+        if (currentState == images.Length - 1 && Modifier == -1)
+        {
+            currentState += Modifier;
+        }
+
+        Modifier = 0;
+
+
+
+        if (currentState < 0)
+        {
+            currentState = 0;
         }
     }
 }
